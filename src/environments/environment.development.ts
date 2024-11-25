c6d547bd18c1e4b33370359ed277a01a132af5ff
Alan58-star <alanmartinez43890@gmail.com>
2024-11-25 04:27:11 -0300

    jiji

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,2 +1,2 @@
 //export const baseUrl = "https://sthdlh-back.onrender.com"
-export const baseUrl = "https://sthdlh-back.onrender.com"
+export const baseUrl = "https://sistema-de-turnos-hdlh-back.onrender.com"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "https://sthdlh-back.onrender.com"
+export const baseUrl = "https://sistema-de-turnos-hdlh-back.onrender.com"
