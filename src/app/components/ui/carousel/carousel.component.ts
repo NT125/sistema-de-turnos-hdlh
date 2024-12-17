b0de70df85a0cda449d4f7901cb80e64aa1bbca7
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2024-12-17 15:59:46 -0300

    Imágenes reducidas

--- src/app/components/ui/carousel/carousel.component.ts
+++ src/app/components/ui/carousel/carousel.component.ts
@@ -19,18 +19,6 @@ export class CarouselComponent {
     { img: "/slider/2.jpg" },
     { img: "/slider/3.jpg" },
     { img: "/slider/4.jpg" },
-    { img: "/slider/5.jpg" },
-    { img: "/slider/6.jpg" },
-    { img: "/slider/7.jpg" },
-    { img: "/slider/8.jpg" },
-    { img: "/slider/9.jpg" },
-    { img: "/slider/10.jpg" },
-    { img: "/slider/11.jpg" },
-    { img: "/slider/12.jpg" },
-    { img: "/slider/13.jpg" },
-    { img: "/slider/14.jpg" },
-    { img: "/slider/15.jpg" },
-    { img: "/slider/16.jpg" },
   ];
 
   getImagen(): string {
