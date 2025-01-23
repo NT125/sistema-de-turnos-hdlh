06a7f5141e1f03286933c188a55a556f641f694e
Leonel19 <leonel19coraite@gmail.com>
2025-01-22 21:44:15 -0300

    correción de recordatorio

--- sistema-back/cron.js
+++ sistema-back/cron.js
@@ -34,7 +34,7 @@ async function procesarTurnos() {
       if (fechaTurno.toDateString() === fechaActual.toDateString()) {
 
         // let doctor = turnos[i].medico_id.apellido
-        if (turnos[i].medico_id != null && turnos[i].paciente_id != null && turnos.estado == 'Ocupado') {
+        if (turnos[i].medico_id != null && turnos[i].paciente_id != null && turnos[i].estado == 'Ocupado') {
           
           wpp.recordatorio(
             turnos[i].paciente_id.telefono,
