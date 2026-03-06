2e9c309d68a8a3a5dc4a10ec23c7a296dabb98fe
Alan58-star <alanmartinez43890@gmail.com>
2026-03-06 18:29:30 -0300

    32

--- sistema-back/utils/whatsapp.js
+++ sistema-back/utils/whatsapp.js
@@ -1,16 +1,21 @@
-const { Client, LocalAuth } = require("whatsapp-web.js");
-const puppeteer = require("puppeteer");
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-  puppeteer: {
-    executablePath: puppeteer.executablePath(),
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox"
-    ],
-  },
-  authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        // Estos argumentos son obligatorios para Render y otros servidores en la nube
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', 
+            '--disable-gpu'
+        ]
+        // Generalmente no necesitas especificar "executablePath" si configuraste la variable de caché correctamente
+    }
 });
 
-module.exports = { client };
+client.initialize();
