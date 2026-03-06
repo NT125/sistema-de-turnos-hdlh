8a4f40338c35264f13b4ac0ab9074d61bfc7d34d
Alan58-star <alanmartinez43890@gmail.com>
2026-03-06 18:21:50 -0300

    12

--- sistema-back/utils/whatsapp.js
+++ sistema-back/utils/whatsapp.js
@@ -1,9 +1,14 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const puppeteer = require("puppeteer");
 
 const client = new Client({
   puppeteer: {
+    executablePath: puppeteer.executablePath(),
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox"
+    ],
   },
   authStrategy: new LocalAuth(),
 });
